7381991db981884e0049b6c819cabdf474bcc362
DESKTOP-OV90E6N\leonk <quangttgcs15205@fpt.edu.vn>
2017-11-23 12:17:54 +0700

    create ISEOABLE

---
+++ ElectronicShop.Model/Abstract/ISeoable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicShop.Model.Abstract
+{
+    public class ISeoable
+    {
+        string MetaKeyWord { get; set; }
+        string MetaDescription { get; set; }
+    }
+}
