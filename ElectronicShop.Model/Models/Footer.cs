9cece160cdd3a3acbf9a37a27212c21b4f73b84b
DESKTOP-OV90E6N\leonk <quangttgcs15205@fpt.edu.vn>
2017-11-23 11:34:02 +0700

    create footer

---
+++ ElectronicShop.Model/Models/Footer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElectronicShop.Model.Models
+{
+    [Table("Footers")]
+    class Footer
+    {
+        [Key]
+        public string ID { set; get;}
+
+        [Required]
+        public string content { set; get; }
+
+    }
+}
